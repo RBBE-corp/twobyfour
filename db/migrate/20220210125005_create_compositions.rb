2e5caf2df1ddb02d4e86a22d06301082b590bd4c
reona kasuya <reon.kasuya@gmail.com>
2022-02-10 21:52:35 +0900

    text to array

--- db/migrate/20220210125005_create_compositions.rb
+++ db/migrate/20220210125005_create_compositions.rb
@@ -1,7 +1,7 @@
 class CreateCompositions < ActiveRecord::Migration[6.1]
   def change
     create_table :compositions do |t|
-      t.text :memory_list
+      t.text :memory_list, array: true, default: []
       t.references :instrumental, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.integer :rep_count
