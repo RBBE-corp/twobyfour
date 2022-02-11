6e1ab1a02169cf32e43b948db1c60adb3b18508a
Babin Bohora <bohorababin1@gmail.com>
2022-02-11 21:42:20 +0900

    user table association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :compositions
+  has_many :playlists
+
   validates :username, presence: true
 end
