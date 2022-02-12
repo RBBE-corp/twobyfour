40270d8022d344f085a028db7ef1c3f9cdf1202d
Byron Lee <meetbyron@gmail.com>
2022-02-12 17:27:39 +0900

    memory_list with associations done

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,4 +1,5 @@
 class Composition < ApplicationRecord
   belongs_to :instrumental
   belongs_to :user
+  belongs_to :memory_list
 end
--- app/models/memory_list.rb
+++ app/models/memory_list.rb
@@ -1,3 +1,4 @@
 class MemoryList < ApplicationRecord
   belongs_to :user
+  has_many :compositions
 end
