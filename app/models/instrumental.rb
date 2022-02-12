cbcfb2046a4e92b36f52b906863660ba4f9ae77c
reona kasuya <reon.kasuya@gmail.com>
2022-02-12 12:33:22 +0900

    fix composition bug

--- app/models/instrumental.rb
+++ app/models/instrumental.rb
@@ -1,5 +1,5 @@
 class Instrumental < ApplicationRecord
-  belongs_to :composition
+  has_many :compositions
   validates :title, :artist, :genre, presence: true
   validates :duration, :bpm, numericality: true
   validate  :duration_cannot_be_less_than_eight_seconds,
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@
 # major lazer (light it up, lean on)
 # white stripes 7 nation army
 # chainsmoker -closer
-# ////////// end /////////////////
+
 
 puts "Cleaning up database"
 puts "......"
@@ -73,7 +73,7 @@ byron = User.create!(
 #   username: "Erika Kawashima"
 # )
 
-# ////////// end /////////////////////////
+# ///////////////////////////////////
 
 # //////// Instrumental seeds ////////////
 
@@ -84,6 +84,7 @@ five_hundred_miles = Instrumental.create!(
   duration: 194.4,
   bpm: 94
 )
+puts "instrumental added"
 
 
 all_the_small_things = Instrumental.create!(
@@ -93,6 +94,7 @@ all_the_small_things = Instrumental.create!(
   duration: 148.8,
   bpm: 149
 )
+puts "instrumental added"
 
 lean_on = Instrumental.create!(
   title: "Lean On",
@@ -101,6 +103,7 @@ lean_on = Instrumental.create!(
   duration: 153.6,
   bpm: 98
 )
+puts "instrumental added"
 
 closer = Instrumental.create!(
   title: "Closer",
@@ -109,6 +112,7 @@ closer = Instrumental.create!(
   duration: 242.4,
   bpm: 95
 )
+puts "instrumental added"
 
 seven_nation_army = Instrumental.create!(
   title: "Seven Nation Army",
@@ -117,6 +121,7 @@ seven_nation_army = Instrumental.create!(
   duration: 211.2,
   bpm:124
 )
+puts "instrumental added"
 
 simple_rap = Instrumental.create!(
   title: "The Simple Rap Song",
@@ -125,6 +130,7 @@ simple_rap = Instrumental.create!(
   duration: 153,
   bpm: 130
 )
+puts "instrumental added"
 
 plastic_love = Instrumental.create!(
   title: "Plastic Love",
@@ -133,6 +139,7 @@ plastic_love = Instrumental.create!(
   duration: 270.6,
   bpm: 102
 )
+puts "instrumental added"
 
 island_in_the_sun = Instrumental.create!(
   title: "Island in the Sun",
@@ -141,6 +148,7 @@ island_in_the_sun = Instrumental.create!(
   duration: 192,
   bpm: 115
 )
+puts "instrumental added"
 
 everybody = Instrumental.create!(
   title: "Everybody",
@@ -149,7 +157,9 @@ everybody = Instrumental.create!(
   duration: 207,
   bpm: 108
 )
-# ////////// end //////////////////////////
+# ////puts "instrumental added"
+
+#////////////////////////////////
 
 # ///////// Compositions Seeds ////////////
 # Composition.create!(
@@ -160,6 +170,7 @@ everybody = Instrumental.create!(
 # )
 
 # We need to determine whether this should be an array of arrays
+puts "composition added"
 
 greetings_composition = Composition.create!(
   memory_list: ["Ohayou gozaimasu","Good morning","Konnichiwa","Hello","Konbanwa","Good evening","Oyasuminasai","Goodnight"],
@@ -168,12 +179,15 @@ greetings_composition = Composition.create!(
   rep_count: 15
 )
 
+puts "composition added"
+
 days_of_the_week_composition = Composition.create!(
   memory_list: ["Getsuyoubi","Monday","Kayoubi","Tuesday","Suiyoubi","Wednesday","Mokuyoubi","Thursday", "Kinyoubi", "Friday"],
   instrumental: closer,
   user: byron,
   rep_count: 10
 )
+puts "composition added"
 
 verbs_composition = Composition.create!(
   memory_list: ["Suru","To do","Miru","To see","Kiku","To listen","Hanasu","To speak", "Iu","To say","Kaku","To write","Taberu","To eat","Nomu","To drink","Aruku","To walk","Hashiru","To run","Suwaru","To sit","Tatsu","To stand"],
@@ -181,27 +195,33 @@ verbs_composition = Composition.create!(
   user: byron,
   rep_count: 20
 )
-# ////////// end //////////////////////////////
+
+puts "composition added"
+# //////////////////////////////////////
 
 
 # /////////// Playlist Seeds //////////////////
+puts "playlist"
 
 greetings_playlist = Playlist.create!(
   name: "Greetings",
   user: byron
 )
+puts "playlist added"
 
 days_of_the_week_playlist = Playlist.create!(
   name: "Days of the Week",
   user: byron
 )
+puts "playlist added"
 
 verbs_playlist = Playlist.create!(
   name: "Japanese Verbs",
   user: byron
 )
+puts "playlist added"
 
-# ///////////// end /////////////////////
+# //////////////////////////////////
 
 # ///////////// PlaylistEntry Seeds /////////////////////
 
@@ -209,16 +229,19 @@ PlaylistEntry.create!(
   composition: greetings_composition,
   playlist: greetings_playlist
 )
+puts "playlist_entry added"
 
 PlaylistEntry.create!(
   composition: days_of_the_week_composition,
   playlist: days_of_the_week_playlist
 )
+puts "playlist_entry added"
 
 PlaylistEntry.create!(
   composition: verbs_composition,
   playlist: verbs_playlist
 )
+puts "playlist_entry added"
 
 # ///////////// end /////////////////////
 
@@ -229,15 +252,19 @@ Score.create!(
   score: 5,
   composition: greetings_composition
 )
+puts "score added"
 
 Score.create!(
   score: 20,
   composition: days_of_the_week_composition
 )
+puts "score added"
 
 Score.create!(
   score: 20,
   composition: verbs_composition
 )
+puts "score added"
 
-# ///////////// end /////////////////////
+puts "Database Seeded!"
+# //////////////////////////////////
