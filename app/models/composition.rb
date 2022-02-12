5a7f8dfb4692eb5184de537b082a511850987102
reona kasuya <reon.kasuya@gmail.com>
2022-02-12 12:01:10 +0900

    validation added for composition and score

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,4 +1,5 @@
 class Composition < ApplicationRecord
   belongs_to :instrumental
   belongs_to :user
+  validates :memory_list, :instrumental_id, :user_id, presence: true
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,4 @@
 class Score < ApplicationRecord
   belongs_to :composition
+  validates :score, :playlist_id, presence: true
 end
