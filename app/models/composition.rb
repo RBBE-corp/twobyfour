7bc47d1568dc31de791ba07ce276ea4bd72a47d9
Babin Bohora <bohorababin1@gmail.com>
2022-02-12 17:52:55 +0900

    score feature for user

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,5 +1,6 @@
 class Composition < ApplicationRecord
   belongs_to :instrumental
   belongs_to :user
+  has_many :scores
   validates :memory_list, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,16 @@ class User < ApplicationRecord
   has_many :playlists, dependent: :destroy
 
   validates :username, presence: true
+
+  def avg_scores
+
+    return 0 unless compositions.count.positive?
+    sum = 0
+    counter = compositions.count;
+    compositions.each do |composition|
+
+      sum = composition.scores.average(:score).round(2).to_f
+    end
+    (sum / counter).round(2)
+  end
 end
