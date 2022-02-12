ddafc878f861e8f32d6c71c97140e52e69a59498
reona kasuya <reon.kasuya@gmail.com>
2022-02-12 13:02:50 +0900

    composition controller created

--- app/controllers/composition_controller.rb
+++ app/controllers/composition_controller.rb
@@ -1,12 +1,46 @@
 class CompositionController < ApplicationController
+  before_action :find_composition, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @compositions = Composition.all
+  end
+
+  def new
+    @composition = Composition.new
+  end
 
   def create
+    @composition = Composition.new(composition_params)
+    @composition.user = current_user
+    @composition.save
+    if @composition.save
+      redirect_to composition_path(@composition)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def update
+    if @composition.update(composition_params)
+      redirect_to composition_path, notice: "Composition created!"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @composition.destroy
   end
 
-  def update
+  private
+
+  def composition_params
+    params.require(:composition).permit(:memory_list)
   end
 
+  def find_composition
+    @composition = Composition.find(params[:id])
+  end
 end
