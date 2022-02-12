c6460850ee4e5ab289d17f3d771e0563a5dcb91c
reona kasuya <reon.kasuya@gmail.com>
2022-02-12 13:49:16 +0900

    controllers created for score and composition

--- app/controllers/score_controller.rb
+++ app/controllers/score_controller.rb
@@ -1,2 +1,31 @@
 class ScoreController < ApplicationController
+  before_action :find_score, only: [:show, :update]
+
+  def new
+    @score = Score.new
+  end
+
+  def create
+    @score = Score.new(score_params)
+    @score.user = current_user
+    @score.save
+  end
+
+  def update
+    if @score.update(score_parms)
+      redirect_to score_path, notice: "Score for composition updated!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:score)
+  end
+
+  def find_score
+    @score = Score.find(params[:id])
+  end
 end
