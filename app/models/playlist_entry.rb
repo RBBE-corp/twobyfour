2e213f27e41d7068e08a49c12fa720b1e8faefb3
Erika Kawashima <marshallekm@gmail.com>
2022-02-12 18:13:18 +0900

    playlists new form functional and playlist destroy action functional

--- app/models/playlist_entry.rb
+++ app/models/playlist_entry.rb
@@ -1,6 +1,4 @@
 class PlaylistEntry < ApplicationRecord
   belongs_to :composition
   belongs_to :playlist
-  validates :composition_id, presence: true
-  validates :playlist_id, presence: true
 end
