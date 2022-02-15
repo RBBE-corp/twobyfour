30c1484b6254fac3050e0590e91427f669faed97
Babin Bohora <bohorababin1@gmail.com>
2022-02-15 22:39:45 +0900

    basic linking of flashcards

--- app/models/memory_list.rb
+++ app/models/memory_list.rb
@@ -1,5 +1,7 @@
 class MemoryList < ApplicationRecord
   belongs_to :user
   has_many :compositions, dependent: :destroy
+  has_many :memory_list_flashcards, dependent: :destroy
+  has_many :flashcards, through: :memory_list_flashcards
   validates :name, presence: true, uniqueness: true
 end
