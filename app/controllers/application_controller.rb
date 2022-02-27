955e74aa80b8acaf42f029e32be42d0f3225f6cc
Babin Bohora <bohorababin1@gmail.com>
2022-02-27 16:59:51 +0900

    underscore remover -> underscore_remover('raw_string')

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  helper_method :underscore_remover
+ 
+  private
+
+  def underscore_remover(raw_string)
+    raw_string.gsub("_", " ")  
+  end
 end
