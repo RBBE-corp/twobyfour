0c8a7dd5cabed3307cb6e6cb8d2499321413e3a4
Babin Bohora <bohorababin1@gmail.com>
2022-02-28 13:34:25 +0900

    removing underscore from composition show and changed the underscore_remover function little bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def underscore_remover(raw_string)
-    raw_string.capitalize!
-    raw_string.gsub("_", " ")  
+    capitalized_string = raw_string.capitalize
+    capitalized_string.gsub("_", " ")  
   end
 end
