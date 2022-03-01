cbf250065c2eb0336bfc0a86d3a13f90f6f925b6
reona kasuya <reon.kasuya@gmail.com>
2022-03-01 18:35:06 +0900

    sortable shared list working for three lists

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -20,5 +20,26 @@ document.addEventListener('turbolinks:load', () => {
 
 // Basic sortable //
 
-var el = document.getElementById('memory-list-items');
-var sortable = Sortable.create(el);
+// var el = document.getElementById('memory-list-items');
+// var sortable = Sortable.create(el);
+
+
+var example2Right = document.getElementById('example2-right')
+var example2Left = document.getElementById('example2-left')
+var example2Middle = document.getElementById('example2-middle')
+
+// Shared list no cloning //
+new Sortable(example2Left, {
+  group: 'shared', // set both lists to same group
+  animation: 150
+});
+
+new Sortable(example2Middle, {
+  group: 'shared',
+  animation: 150
+});
+
+new Sortable(example2Right, {
+  group: 'shared',
+  animation: 150
+});
