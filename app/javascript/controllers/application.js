f6c78160447cef6275068c3bd7c27e48061c3a89
reona kasuya <reon.kasuya@gmail.com>
2022-03-01 17:54:12 +0900

    sortable configuration set up complete

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,3 +1,4 @@
+
 import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
@@ -7,3 +8,17 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+
+// Configuration for sortable js //
+import Sortable from 'sortablejs';
+
+// Checking if library is loaded
+document.addEventListener('turbolinks:load', () => {
+  console.log("Sortable: ", Sortable);
+})
+
+// Basic sortable //
+
+var el = document.getElementById('memory-list-items');
+var sortable = Sortable.create(el);
