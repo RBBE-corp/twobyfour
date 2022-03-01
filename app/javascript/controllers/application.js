ac2dedb81e1af96f109b79adbc73d09d7e9cb4c6
reona kasuya <reon.kasuya@gmail.com>
2022-03-01 22:16:37 +0900

    end of day 3/1

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -90,6 +90,7 @@ new Sortable.create(C, {
       //       evt.clone, // the clone element
       //       evt.pullMode
     ]);
+
   }
 });
 
@@ -113,3 +114,31 @@ new Sortable.create(D, {
     ]);
   }
 });
+
+
+// const insert = document.querySelector('#insert');
+// insert.addEventListener('click', () => {
+//   // subject is what gets inserted, so it needs to be the memorylist_id
+//   const subject = document.querySelector('#subject');
+//   const positionSelect = document.querySelector('#position');
+//   subject.insertAdjacentHTML(positionSelect.value, '<strong>inserted text</strong>');
+// });
+
+// const reset = document.querySelector('#reset');
+// reset.addEventListener('click', () => {
+//   document.location.reload();
+// });
+
+
+// const insert = document.querySelector('#insert');
+// insert.addEventListener('click', () => {
+//   // subject is what gets inserted, so it needs to be the memorylist_id
+//   const subject = document.querySelector('#subject');
+//   const positionSelect = document.querySelector('#composition_memory_list_id');
+//   subject.insertAdjacentHTML(positionSelect.value, '<strong>inserted text</strong>');
+// });
+
+// const reset = document.querySelector('#reset');
+// reset.addEventListener('click', () => {
+//   document.location.reload();
+// });
