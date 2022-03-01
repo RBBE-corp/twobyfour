14e49095b9eb3a9a79d3038ffc40bef155c7d0d8
reona kasuya <reon.kasuya@gmail.com>
2022-03-01 21:06:39 +0900

    core lists made

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -14,38 +14,64 @@ export { application }
 import Sortable from 'sortablejs';
 
 // Checking if library is loaded
-document.addEventListener('turbolinks:load', () => {
-  console.log("Sortable: ", Sortable);
-})
+// document.addEventListener('turbolinks:load', () => {
+//   console.log("Sortable: ", Sortable);
+// })
 
 // Basic sortable //
-
 // var el = document.getElementById('memory-list-items');
 // var sortable = Sortable.create(el);
 
 var A = document.getElementById('A')
-var example2Right = document.getElementById('example2-right')
-var example2Left = document.getElementById('example2-left')
-var example2Middle = document.getElementById('example2-middle')
-
-// Shared list no cloning //
-new Sortable(example2Left, {
-  group: 'shared', // set both lists to same group
-  animation: 150
-});
-
-new Sortable(example2Middle, {
-  group: 'shared',
-  animation: 150
-});
-
-new Sortable(example2Right, {
-  group: 'shared',
-  animation: 150,
-  swap: true
-});
+var B = document.getElementById('B')
+var C = document.getElementById('C')
 
 new Sortable.create(A, {
+  swap: true,
+  group: {
+    name: "shared",
+  },
+  sort: true,
+  onEnd: function (evt) {
+    console.log([
+      evt.item.id,  // dragged HTMLElement
+      //       evt.to,    // target list
+      //       evt.from,  // previous list
+      //       evt.oldIndex,  // element's old index within old parent
+      //       evt.newIndex,  // element's new index within new parent
+      //       evt.oldDraggableIndex, // element's old index within old parent, only counting draggable elements
+      //       evt.newDraggableIndex, // element's new index within new parent, only counting draggable elements
+      //       evt.clone, // the clone element
+      //       evt.pullMode
+    ]);
+  }
+});
+
+
+new Sortable.create(B, {
+  swap: true,
+  group: {
+    name: "shared",
+  },
+  sort: true,
+  onEnd: function (evt) {
+    console.log([
+      evt.item.id,  // dragged HTMLElement
+      //       evt.to,    // target list
+      //       evt.from,  // previous list
+      //       evt.oldIndex,  // element's old index within old parent
+      //       evt.newIndex,  // element's new index within new parent
+      //       evt.oldDraggableIndex, // element's old index within old parent, only counting draggable elements
+      //       evt.newDraggableIndex, // element's new index within new parent, only counting draggable elements
+      //       evt.clone, // the clone element
+      //       evt.pullMode
+    ]);
+  }
+});
+
+
+
+new Sortable.create(C, {
   swap: true,
   group: {
     name: "shared",
