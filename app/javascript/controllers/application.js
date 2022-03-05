4e5e319a7a2c741b455234e43a4ff28d04b85449
Babin Bohora <bohorababin1@gmail.com>
2022-03-05 13:20:47 +0900

    cool

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -10,97 +10,97 @@ window.Stimulus   = application
 export { application }
 
 // Configuration for sortable js //
-import Sortable from 'sortablejs';
+// import Sortable from 'sortablejs';
 
-// Checking if library is loaded
-// document.addEventListener('turbolinks:load', () => {
-//   console.log("Sortable: ", Sortable);
-// })
+// // Checking if library is loaded
+// // document.addEventListener('turbolinks:load', () => {
+// //   console.log("Sortable: ", Sortable);
+// // })
 
-// Basic sortable syntax//
-// var el = document.getElementById('memory-list-items');
-// var sortable = Sortable.create(el);
+// // Basic sortable syntax//
+// // var el = document.getElementById('memory-list-items');
+// // var sortable = Sortable.create(el);
 
-var A = document.getElementById('A') //Memorylist
-var B = document.getElementById('B') //Instrumentals
-var C = document.getElementById('C') //Memorylist Catcher
-var D = document.getElementById('D') //Instrumentals Catcher
-// A + C memorylist-shared
-// B + D instrumental-shared
+// var A = document.getElementById('A') //Memorylist
+// var B = document.getElementById('B') //Instrumentals
+// var C = document.getElementById('C') //Memorylist Catcher
+// var D = document.getElementById('D') //Instrumentals Catcher
+// // A + C memorylist-shared
+// // B + D instrumental-shared
 
-var memorylistinput = document.getElementById('composition_memory_list_id')
-var instrumentalinput = document.getElementById('composition_instrumental_id')
+// var memorylistinput = document.getElementById('composition_memory_list_id')
+// var instrumentalinput = document.getElementById('composition_instrumental_id')
 
-// Memorylist //
-new Sortable.create(A, {
-  swap: true,
-  swapThreshold: 1,
-  group: {
-    name: "memorylist-shared",
-  },
-  sort: true,
-  onEnd: function (evt) {
+// // Memorylist //
+// new Sortable.create(A, {
+//   swap: true,
+//   swapThreshold: 1,
+//   group: {
+//     name: "memorylist-shared",
+//   },
+//   sort: true,
+//   onEnd: function (evt) {
 
-    memorylistinput.value = evt.item.id;
-    console.log([
-      evt.item.id,
-    ]);
-  }
-});
+//     memorylistinput.value = evt.item.id;
+//     console.log([
+//       evt.item.id,
+//     ]);
+//   }
+// });
 
-// Instrumentals //
-new Sortable.create(B, {
-  swap: true,
-  swapThreshold: 1,
-  group: {
-    name: "instrumental-shared",
-  },
-  sort: true,
-  onEnd: function (evt) {
-    instrumentalinput.value = evt.item.id;
-    console.log([
-      evt.item.id,  // dragged HTMLElement
-    ]);
-  }
-});
+// // Instrumentals //
+// new Sortable.create(B, {
+//   swap: true,
+//   swapThreshold: 1,
+//   group: {
+//     name: "instrumental-shared",
+//   },
+//   sort: true,
+//   onEnd: function (evt) {
+//     instrumentalinput.value = evt.item.id;
+//     console.log([
+//       evt.item.id,  // dragged HTMLElement
+//     ]);
+//   }
+// });
 
-// Memorylist Catcher //
-new Sortable.create(C, {
-  swap: true,
-  swapThreshold: 1,
-  group: {
-    name: "memorylist-shared",
-  },
-  sort: true,
-  onEnd: function (evt) {
-    console.log([
-      evt.item.id,
-    ]);
-  }
-});
+// // Memorylist Catcher //
+// new Sortable.create(C, {
+//   swap: true,
+//   swapThreshold: 1,
+//   group: {
+//     name: "memorylist-shared",
+//   },
+//   sort: true,
+//   onEnd: function (evt) {
+//     console.log([
+//       evt.item.id,
+//     ]);
+//   }
+// });
 
-// Instrumentals Catcher //
-new Sortable.create(D, {
-  swap: true,
-  swapThreshold: 1,
-  group: {
-    name: "instrumental-shared",
-  },
-  sort: true,
-  onEnd: function (evt) {
-    console.log([
-      evt.item.id,  // dragged HTMLElement
-      //       evt.to,    // target list
-      //       evt.from,  // previous list
-      //       evt.oldIndex,  // element's old index within old parent
-      //       evt.newIndex,  // element's new index within new parent
-      //       evt.oldDraggableIndex, // element's old index within old parent, only counting draggable elements
-      //       evt.newDraggableIndex, // element's new index within new parent, only counting draggable elements
-      //       evt.clone, // the clone element
-      //       evt.pullMode
-    ]);
-  }
-});
+// // Instrumentals Catcher //
+// new Sortable.create(D, {
+//   swap: true,
+//   swapThreshold: 1,
+//   group: {
+//     name: "instrumental-shared",
+//   },
+//   sort: true,
+//   onEnd: function (evt) {
+//     console.log([
+//       evt.item.id,  // dragged HTMLElement
+//       //       evt.to,    // target list
+//       //       evt.from,  // previous list
+//       //       evt.oldIndex,  // element's old index within old parent
+//       //       evt.newIndex,  // element's new index within new parent
+//       //       evt.oldDraggableIndex, // element's old index within old parent, only counting draggable elements
+//       //       evt.newDraggableIndex, // element's new index within new parent, only counting draggable elements
+//       //       evt.clone, // the clone element
+//       //       evt.pullMode
+//     ]);
+//   }
+// });
 
 // How it works: JS sortable to fill in a form //
 // 1. Sortable.create makes a sortabls list
