b3151ccb408e636879edeca151e20f13b1055291
Byron Lee <meetbyron@gmail.com>
2022-03-07 22:45:17 +0900

    memory list and flashcard front end fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_122753) do
+ActiveRecord::Schema.define(version: 2022_03_07_000230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_02_13_122753) do
     t.datetime "last_practiced"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "furigana"
   end
 
   create_table "instrumentals", force: :cascade do |t|
