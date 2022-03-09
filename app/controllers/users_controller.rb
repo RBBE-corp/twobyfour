07c3d7113d05c6d82e6ee00111cca17475abdf3b
Erika Kawashima <marshallekm@gmail.com>
2022-03-10 06:57:11 +0900

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def profile
-    @compositions = current_user.compositions
+    @compositions = current_user.compositions.page(params[:page]).per(3)
     @playlists = current_user.playlists
     @scores = current_user.scores.order(created_at: :desc)
     @memory_lists = current_user.memory_lists
     @composition = Composition.order(rep_count: :asc).first
     @data = Composition.joins(:scores).group_by_day('compositions.created_at').sum('scores.score')
+  end
+
+  def average_rating
+    @average_score = current_user.compositions.average(:score)
   end
 end
