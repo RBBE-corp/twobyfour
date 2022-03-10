3f60cc3d5f2e5d9b758915385626b8e7b902ef9e
Byron Lee <meetbyron@gmail.com>
2022-03-10 20:25:18 +0900

    project checklist tasks done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   helper_method :underscore_remover
- 
+
   private
 
   def underscore_remover(raw_string)
     capitalized_string = raw_string.capitalize
-    capitalized_string.gsub("_", " ")  
+    capitalized_string.gsub("_", " ")
   end
+end
+
+def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
 end
