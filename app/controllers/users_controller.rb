2173abd4794567daf0a3aabd6f69b7492932f552
Babin Bohora <bohorababin1@gmail.com>
2022-03-29 13:27:27 +0900

    first 3 memorylist as global memorylist for all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
     @compositions = current_user.compositions
     @playlists = current_user.playlists
     @scores = current_user.scores.order(created_at: :desc)
-    @memory_lists = current_user.memory_lists
+
+    # first 3 memorylists and memorylists associate with current user
+    @memory_lists = MemoryList.first(3).concat current_user.memory_lists
+    
     # least reps
     @composition = current_user.compositions&.first
     # sum of compo score  s by day
